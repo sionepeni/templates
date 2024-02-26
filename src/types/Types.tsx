47ba0feacbbb6bd31c8e076147a1acf2a062dabe
Vin274 <101485986+Vin274@users.noreply.github.com>
2024-02-26 17:11:49 +1300

    added types and templates.tsx

---
+++ src/types/Types.tsx
@@ -0,0 +1,13 @@
+export type BasicForm = {
+    user: string,
+    ticket: string,
+    oldDevice: string,
+    newDevice: string
+    id: string,
+    text: string,
+    supervisor: string,
+    laptop: string,
+    mobile: string,
+    checkbox: boolean,
+    switch: boolean
+}
