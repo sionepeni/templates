38478c0daa07527050129016fbfa8ab041ce8fe4
Vin274 <101485986+Vin274@users.noreply.github.com>
2024-02-26 17:01:32 +1300

    added list for input arrays and buttons

---
+++ src/components/List.tsx
@@ -0,0 +1,17 @@
+export const listForButtons = [
+    {id: 'Phone Replacement Notes', index: 0},
+    {id: 'Laptop Replacement Notes', index: 1},
+    {id: 'Out of life cycle Email', index: 2},
+    {id: 'New, replacement Email', index: 3},
+    {id: 'Leaver Template', index: 4}]
+
+export const listForInputs = [
+    {id: 'user', text: 'User'},
+    {id: 'ticket', text: 'Ticket'},
+    {id: 'oldDevice', text: 'Old Device'},
+    {id: 'newDevice', text: 'New Deivce'}]
+
+export const listForLeaverInputs = [
+    {id: 'mobile', text: 'Phone', checkbox: true},
+    {id: 'laptop', text: 'Laptop', checkbox: true},
+    {id: 'supervisor', text: 'Supervisor', checkbox: false}]
