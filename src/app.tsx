ce45dd2b4de2758faaa49dc0d79b3e2876f35eca
Vin Sionepeni <sionepv@aklc.govt.nz>
2024-09-18 13:55:34 +1200

    added content component

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,21 @@
 import { useState } from 'preact/hooks';
-import { EmailTemplates } from './pages/EmailTemplates';
-import { TicketNotes } from './pages/TicketNotes';
 import "./app.css"
 import { JSX } from 'preact/jsx-runtime';
-import { LeaversTemplates } from './pages/LeaversTemplate';
-import { NewStarterEmail } from './pages/NewStarterEmail';
 import { Header } from './components/header/Header';
+import { Content } from './components/content/Content';
 
 export function App() {
 
-    const [formToRender, setFormToRender] = useState<JSX.Element>(<TicketNotes />)
+    const [selectedValue, setSelectedValue] = useState('')
 
     const handlePageSelection = (e: JSX.TargetedEvent<HTMLButtonElement>) =>{
-        if (e.currentTarget.value === '1') return setFormToRender(<TicketNotes />)
-            if (e.currentTarget.value === '2') return setFormToRender(<EmailTemplates />)
-            if (e.currentTarget.value === '3') return setFormToRender(<LeaversTemplates />)
-            if (e.currentTarget.value === '4') return setFormToRender(<NewStarterEmail />)
+        setSelectedValue(e.currentTarget.value)
     }
 
     return (
         <>
         <Header selectPageDetails={handlePageSelection}/>
-            {formToRender}
+        <Content selectedValue={selectedValue}/>
         </>
     )
 }
---
+++ src/components/content/Content.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "preact/hooks"
+import { JSX } from "preact/jsx-runtime"
+import { TicketNotes } from "../../pages/TicketNotes"
+import { EmailTemplates } from "../../pages/EmailTemplates"
+import { LeaversTemplates } from "../../pages/LeaversTemplate"
+import { NewStarterEmail } from "../../pages/NewStarterEmail"
+
+export const Content = ({selectedValue}: any) => {
+
+    const [formToRender, setFormToRender] = useState<JSX.Element>(<TicketNotes />)
+
+    useEffect(() => {
+    handlePageSelection(selectedValue)
+    }, [selectedValue])
+
+    const handlePageSelection = (e: string) =>{
+        if (e === '1') return setFormToRender(<TicketNotes />)
+            if (e === '2') return setFormToRender(<EmailTemplates />)
+            if (e === '3') return setFormToRender(<LeaversTemplates />)
+            if (e === '4') return setFormToRender(<NewStarterEmail />)
+    }
+
+    return(
+        <>
+        {formToRender}
+        </>
+    )
+}
