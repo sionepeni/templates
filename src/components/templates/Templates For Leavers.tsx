df1806fe73b87a3ac4fb20a3463b4124f38bc357
Vin Sionepeni <sionepv@aklc.govt.nz>
2025-03-12 09:22:55 +1300

    refactored css file, added line break

--- src/components/templates/Templates For Leavers.tsx
+++ src/components/templates/Templates For Leavers.tsx
@@ -5,20 +5,19 @@ interface LeaverProps2 {
     user: string;
     ticket: string;
     supervisor: string;
-    equipment: { [key: string]: string }; // Dynamic equipment list
+    equipment: { [key: string]: string };
 }
 
 export const TemplatesForLeavers = (userForm: LeaverProps2) => {
     const address = HandleEmailAddress(userForm.supervisor);
     const subject = encodeURIComponent(`${userForm.ticket} - ${userForm.user} - Equipment Return`);
-    const hello = Greeting(userForm.supervisor); // Use the updated Greeting function
-
-    // Generate equipment list dynamically, with each item on its own line
+    const hello = Greeting(userForm.supervisor); 
+    
     const equipmentList = Object.entries(userForm.equipment)
-        .map(([key, value]) => `${key}: ${value}`) // Format each equipment entry
-        .join('\n'); // Join with line breaks
+        .map(([key, value]) => `${key}: ${value}`)
+        .join('\n'); 
 
-    const emailBody = `${hello}I am writing to follow up regarding a user, ${userForm.user}, who has yet to return the following ICT equipment:\n\n${equipmentList}\n\nPlease note that according to our ICT policy, failure to return the aforementioned items will result in a charge to your cost department.\n\nShould you have any inquiries or concerns regarding this matter, please do not hesitate to inform me.\n\nThank you for your attention to this matter.`;
+    const emailBody = `${hello}\nI am writing to follow up regarding a user, ${userForm.user}, who has yet to return the following ICT equipment:\n\n${equipmentList}\n\nPlease note that according to our ICT policy, failure to return the aforementioned items will result in a charge to your cost department.\n\nShould you have any inquiries or concerns regarding this matter, please do not hesitate to inform me.\n\nThank you for your attention to this matter.`;
 
     const mailtoLink = `mailto:${address}?subject=${subject}&body=${encodeURIComponent(emailBody)}`;
 
